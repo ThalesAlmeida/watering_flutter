9798c2b56ee5644c330fe546279c1d5c2ac34166
Thales <thalesalves44@gmail.com>
2018-12-23 10:52:05 -0300

    adicionando verificação e permitindo o usuário de atualizar uma propriedade

--- lib/screens/propriety_screen.dart
+++ lib/screens/propriety_screen.dart
@@ -28,9 +28,9 @@ class _ProprietyPageState extends State<ProprietyPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    if(widget.propriety == null){
+    if (widget.propriety == null) {
       _editedPropriety = Propriety();
-    }else{
+    } else {
       _editedPropriety = Propriety.fromMap((widget.propriety.toMap()));
       _nomeController.text = _editedPropriety.name;
       _latitudeController.text = _editedPropriety.latitude;
@@ -40,63 +40,95 @@ class _ProprietyPageState extends State<ProprietyPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_editedPropriety.name ?? "Nova Propriedade"),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            if(_editedPropriety.name != null && _editedPropriety.name.isNotEmpty){
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_editedPropriety.name ?? "Nova Propriedade"),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedPropriety.name != null &&
+                _editedPropriety.name.isNotEmpty) {
               Navigator.pop(context, _editedPropriety);
-            }else{
+            } else {
               FocusScope.of(context).requestFocus(_nameFocus);
             }
           },
-        child: Icon(Icons.save),
+          child: Icon(Icons.save),
           backgroundColor: Colors.green,
-      ),
-      body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  controller: _nomeController,
-                  decoration: InputDecoration(labelText: "Nome da propriedade"),
-                  onChanged: (text){
-                    _userEdited = true;
-                    setState(() {
-                      _editedPropriety.name = text;
-                    });
-                  },
-                ),
-                TextField(
-                  controller: _latitudeController,
-                  decoration: InputDecoration(labelText: "Latitude"),
-                  onChanged: (text){
-                    _userEdited = true;
-                    _editedPropriety.latitude = text;
-                  },
-                  keyboardType: TextInputType.numberWithOptions(),
-                ),
-                TextField(
-                  controller: _sandController,
-                  decoration: InputDecoration(labelText: "Nível de areia"),
-                  onChanged: (text){
-                    _userEdited = true;
-                    _editedPropriety.sand = text;
-                  },
-                  keyboardType: TextInputType.numberWithOptions(),
-                ),
-              ],
-            ),
-          )
+        ),
+        body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: _nomeController,
+                    decoration: InputDecoration(
+                        labelText: "Nome da propriedade"),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      setState(() {
+                        _editedPropriety.name = text;
+                      });
+                    },
+                  ),
+                  TextField(
+                    controller: _latitudeController,
+                    decoration: InputDecoration(labelText: "Latitude"),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      _editedPropriety.latitude = text;
+                    },
+                    keyboardType: TextInputType.numberWithOptions(),
+                  ),
+                  TextField(
+                    controller: _sandController,
+                    decoration: InputDecoration(labelText: "Nível de areia"),
+                    onChanged: (text) {
+                      _userEdited = true;
+                      _editedPropriety.sand = text;
+                    },
+                    keyboardType: TextInputType.numberWithOptions(),
+                  ),
+                ],
+              ),
+            )
+        ),
       ),
     );
   }
 
-  _requestPop(){
-
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Descartar Alterações?"),
+              content: Text("Se sair as alterações serão perdidas."),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Cancelar"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                  child: Text("Sim"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            );
+          }
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
